9560a30e2a5c4252397687785d0aaa699c1fbce7
unknown <kyu9iro@gmail.com>
2020-09-08 03:18:31 +0900

    add all

--- PointSymmetry/main.js
+++ PointSymmetry/main.js
@@ -1,13 +1,14 @@
 var canvas, ctx, width, height;
 ctx = (canvas = document.getElementById("canvas")).getContext("2d");
 
+var back_color = "black";
 width = canvas.width = window.innerWidth;
 height = canvas.height = window.innerHeight;
-canvas.style = "position: absolute; top: 0; left: 0;";
+canvas.style = "position: absolute; top: 0; left: 0; background-color: back_color;";
 
 var box = {
-	size: 20,
-	color: "black",
+	size: 10,
+	color: "rgb(192, 192, 192)",
 	graph: function(x, y) {
 		ctx.fillRect(x * this.size, y * this.size, this.size, this.size);
 	}
@@ -24,42 +25,53 @@ for (var i=0; (i + 1) * box.size < o.w; i++) {
 	cnt++;
 }
 
+function getAdjacentLivesCount(tmp, _x, _y) {
+	var count = 0;
+	for (var y=-1; y<=1; y++)
+		for (var x=-1; x<=1; x++) {
+			if ((x == 0) && (y == 0))
+				continue;
+			
+			var x2 = (cnt + _x + x) % cnt;
+			var y2 = (cnt + _y + y) % cnt;
+			count += tmp[x2][y2];
+		}
+	
+	return count;
+}
 function update() {
-	var tmp = board.map(x => x);
+	/*for (var i=0; i<cnt; i++) {
+		for (var j=0; j<cnt-1; j++) {
+			board[i][j] = board[i][j+1];
+		}
+		board[i][j] = 1;
+	}*/
+	var tmp = board.map(x => x.map(a => 0));
 	
 	for (var i=0; i<cnt; i++) {
 		for (var j=0; j<cnt; j++) {
-			var find = 0;
-			for (var k=-1; k<2; k++) {
-				for (var l=-1; l<2; l++) {
-					if (!(k==0 && l==0) && (0 <= i+k && i+k < board.length) && (0 <= j+l && j+l < board.length)) {
-						if (tmp[i+k][j+l] == 1) {
-							find++;
-						}
-					}
-				}
+			var find = getAdjacentLivesCount(board, i, j);
+			
+			var next = board[i][j];
+			if (board[i][j]) {
+				if (find <= 1 || find >= 4)
+					next = 0;
 			}
-			if (tmp[i][j] == 0) {
-				if (find == 3) {
-					board[i][j] = 1;
-				}
-			} else {
-				if (find == 2 || find == 3) {
-					board[i][j] = 1;
-				}
-				if (find <= 1 || 4 <= find) {
-					board[i][j] = 0;
-				}
+			else {
+				if (find == 3)
+					next = 1;
 			}
+			tmp[i][j] = next;
 		}
 	}
+	board = tmp.map(x => x);
 	
-	ctx.fillStyle = "white";
+	ctx.fillStyle = back_color;
 	ctx.fillRect(0, 0, width, height);
 	ctx.fillStyle = box.color;
 	for (var i=0; i<cnt; i++) {
 		for (var j=0; j<cnt; j++) {
-			if (board[i][j] == 1) {
+			if (board[i][j]) {
 				box.graph(o.x / box.size - i - 1, o.y / box.size - j - 1);
 				box.graph(o.x / box.size - j - 1, o.y / box.size + i);
 				box.graph(o.x / box.size + j, o.y / box.size - i - 1);
@@ -71,5 +83,7 @@ function update() {
 
 (function() {
 	update();
-	setInterval(function(){update();}, 500);
+	setInterval(function(){update();}, 100);
 })();
+
+//canvas.addEventListener("click", function() { update(); });
