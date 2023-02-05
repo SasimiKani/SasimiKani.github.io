bafba787574143f7ed2a6678b45dc12a86b0acdc
SasimiKani <kyu9iro@gmail.com>
2023-02-05 22:08:13 +0900

    add all

--- tetris/script.js
+++ tetris/script.js
@@ -1,11 +1,17 @@
 var canvas, ctx, width, height;
-canvas = document.querySelector("canvas");
+canvas = document.querySelector("canvas#main");
+notice = document.querySelector("canvas#notice");
 ctx = canvas.getContext("2d");
+noticeCtx = notice.getContext("2d");
 
-var blockSize = 20, hSize = 20, vSize = 30;
+var blockSize = 30, hSize = 10, vSize = 20;
 width = canvas.width = blockSize * hSize;
 height = canvas.height = blockSize * vSize;
-canvas.style = "border: 1px solid black;";
+canvas.style = "border: 1px solid black; float:left;";
+
+notice.width = blockSize * 6;
+notice.height = blockSize * 4;
+notice.style = "border: 1px solid black; float:left; margin-left: 5px;";
 
 var blocks = [
 	[[0, 1], [1, 0], [1, 1], "yellow", "O"],
@@ -20,12 +26,17 @@ var colors = ["yellow", "lightblue", "blue", "orange", "green", "red", "violet"]
 
 var fieldBlocks = [];
 var now;
+var noticeBlock;
 function pushBlock() {
+	now = noticeBlock;
+	pushNotice();
+}
+function pushNotice() {
 	var type = blocks[parseInt(Math.random() * blocks.length)];
-	now = [5, 0, Object.assign(type), 0];
+	noticeBlock = [5, 0, Object.assign(type), 0];
 }
 
-function putBlock(x, y) {
+function putBlock(x, y, ctx) {
 	ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
 	ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
 }
@@ -33,9 +44,18 @@ function viewBlock(x, y, tBlock, r) {
 	var tBlock = rotateBlock(tBlock, r);
 	
 	ctx.fillStyle = tBlock[3];
-	putBlock(x, y);
+	putBlock(x, y, ctx);
 	for (var i=0; i<3; i++) {
-		putBlock(x + tBlock[i][0], y + tBlock[i][1]);
+		putBlock(x + tBlock[i][0], y + tBlock[i][1], ctx);
+	}
+}
+function viewNoticeBlock() {
+	var tBlock = noticeBlock[2];
+	
+	noticeCtx.fillStyle = tBlock[3];
+	putBlock(2, 1, noticeCtx);
+	for (var i=0; i<3; i++) {
+		putBlock(2 + tBlock[i][0], 1 + tBlock[i][1], noticeCtx);
 	}
 }
 function rotateBlock(tBlock, r) {
@@ -52,32 +72,35 @@ function rotateBlock(tBlock, r) {
 	
 	var isOutRange = [0, 0];
 	for (var i=0; i<3; i++) {
-		var x = Math.round(tBlock[i][0] * -Math.cos(ang) + tBlock[i][1] * Math.sin(ang));
+		var x = -Math.round(tBlock[i][0] * -Math.cos(ang) + tBlock[i][1] * Math.sin(ang));
 		var y = Math.round(tBlock[i][0] * Math.sin(ang) + tBlock[i][1] * Math.cos(ang));
 		newBlock.push([x, y]);
 		
-		if (!isOutRange[0]) {
-			if (x + now[0] < 0 || fieldBlocks[y + now[1]][x + now[0]] > -1 && x < 0) isOutRange[0] = 1;
-			if (hSize <= x + now[0] || fieldBlocks[y + now[1]][x + now[0]] > -1 && x > 0) isOutRange[0] = -1;
+		try {
+			while (y + now[1] < 0) {
+				now[1]++;
+			}
+			while (x + now[0] < 0 || fieldBlocks[y + now[1]][x + now[0]] > -1 && x < 0) {
+				now[0]++;
+			}
+			while (hSize <= x + now[0] || fieldBlocks[y + now[1]][x + now[0]] > -1 && x > 0) {
+				now[0]--;
+			}
+		} catch {
+			console.log("y: " + y + " now[1]: " + now[1]);
 		}
-		if (!isOutRange[1] && y + now[1] < 0) isOutRange[1] = 1;
-	}
-	
-	if (isOutRange[0]) {
-		now[0] += isOutRange[0];
-	}
-	if (isOutRange[1]) {
-		now[1]++;
 	}
 	
 	newBlock.push(tBlock[3]);
 	return newBlock;
 }
 
+var deletedLines = 0;
 function deleteLine() {
 	for (var i=0; i<vSize; i++) {
 		if (fieldBlocks[i].indexOf(-1) == -1) {
 			fieldBlocks[i] = fieldBlocks[i].map(n => -1);
+			deletedLines++;
 			for (var y=i; y>0; y--) {
 				fieldBlocks[y] = fieldBlocks[y-1];
 			}
@@ -87,6 +110,9 @@ function deleteLine() {
 
 function main() {
 	ctx.clearRect(0, 0, width, height);
+	noticeCtx.clearRect(0, 0, width, height);
+	
+	document.querySelector("#lines").innerText = deletedLines;
 	
 	ctx.fillStyle = "lightgray";
 	ctx.fillRect(0, 0, width, height);
@@ -108,12 +134,23 @@ function main() {
 	for (var i=0; i<hSize; i++) {
 		for (var j=0; j<vSize; j++) {
 			if (fieldBlocks[j][i] > -1) {
-				ctx.fillStyle = colors[fieldBlocks[j][i]];
-				putBlock(i, j);
+				var line = fieldBlocks[j].map(b => b > -1 ? 0 : -1);
+				var nBlock = rotateBlock(now[2], now[3]) ;
+				if (now[1] == j) line[now[0]] = 0;
+				for (var k=0; k<3; k++) {
+					if (nBlock[k][1] + now[1] == j) line[nBlock[k][0] + now[0]] = 0;
+				}
+				if (line.indexOf(-1) == -1) {
+					ctx.fillStyle = "white";
+				} else {
+					ctx.fillStyle = colors[fieldBlocks[j][i]];
+				}
+				putBlock(i, j, ctx);
 			}
 		}
 	}
 	viewBlock(now[0], now[1], now[2], now[3]);
+	viewNoticeBlock()
 	
 	requestAnimationFrame(main);
 }
@@ -147,7 +184,9 @@ function decision() {
 		}
 		deleteLine();
 		pushBlock();
+		return true;
 	}
+	return false;
 }
 
 addEventListener("keydown", function(e) {
@@ -174,17 +213,16 @@ addEventListener("keydown", function(e) {
 		if (checkRange(+1)) now[0]++;
 		break;
 	case 40: // ↓
+		decision();
 		now[1]++;
 		break;
 	case 90: // Z
-		now[3] = (now[3] + 1) % 4;
-		break;
-	case 88: // X
 		now[3] = (now[3] + 3) % 4;
 		break;
+	case 88: // X
+		now[3] = (now[3] + 1) % 4;
+		break;
 	}
-	
-	decision();
 });
 
 for (var i=0; i<vSize; i++) {
@@ -193,5 +231,12 @@ for (var i=0; i<vSize; i++) {
 		fieldBlocks[i].push(-1);
 	}
 }
+pushNotice();
 pushBlock();
 main();
+
+setInterval(function() {
+	if (!decision()) {
+		now[1]++;
+	}
+}, 1000);
